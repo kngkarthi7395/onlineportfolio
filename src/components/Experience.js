9d2abbf8f90b63d884fd491aeb8e223c4faf27c8
kngkarthi7 <kngkarthi.7@gmail.com>
2022-10-23 12:19:29 +0530

    Added my info to portfolio

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div id = "eduexp" className="experience">
+    <div id="eduexp" className="experience">
       <div className="d-flex justify-content-center my-5">
         <h1>experience and education</h1>
       </div>
@@ -10,48 +10,63 @@ const Experience = () => {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2014-2015</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>2008-2010</h3>
+            <h3>X-Standard</h3>
+            <p>Vidyaa Vikas Educational Institutions to provide the best possible education to its students. Our primary aim is to develop in the student qualities of integrity, honesty, trust, tolerance and compassion, to promote a spirit of enquiry, to foster a scientific temper within the bonds of humanism, to help the student to become a meaningful part of his environment and to know that courage and industry have their due reward.</p>
           </div>
         </div>
         {/* - */}
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2014-2015</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>2010-2012</h3>
+            <h3>XII-Standard</h3>
+            <p>Vidyaa Vikas Educational Institutions to provide the best possible education to its students. Our primary aim is to develop in the student qualities of integrity, honesty, trust, tolerance and compassion, to promote a spirit of enquiry, to foster a scientific temper within the bonds of humanism, to help the student to become a meaningful part of his environment and to know that courage and industry have their due reward.</p>
           </div>
         </div>
         {/* - */}
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2015-2016</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>2012-2016</h3>
+            <h3>B.Tech Information Technology</h3>
+            <p>KCE was established with the vision of providing cutting edge technical education and to create proficient engineers. The college is accredited by NAAC with <b>A</b> Grade, TCS, Wipro and NBA . KCE strives to impart quality education and an excellent career start to all its students.</p>
           </div>
         </div>
         {/* - */}
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2016-2017</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>2016-2018</h3>
+            <h3>Nexge Technologies</h3>
+            <p>Nexge provides diverse range of Products like
+              VoIP, SMS, VAS based Platforms to more than 200
+              Telecom Service Providers in 24 countries serving
+              more than 30 Billion minutes per Annum</p>
           </div>
         </div>
         {/* - */}
         <div className="timeline-block timeline-block-right">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2017-2018</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
-          </div>
-        </div>
-        {/* - */}
-        <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
             <h3>2018-2020</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>Banca Sella</h3>
+            <p>Banca sella is the Group's commercial bank
+              founded in 1886 in Italy . It is recognized in the
+              industry for its excellence in payment system
+              innovation, electronic commerce and private
+              banking
+            </p>
+          </div>
+        </div>
+        {/* - */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2020-2021</h3>
+            <h3>Verizon</h3>
+            <p>Verizon makes it easy for newcomers to the US to use their
+              good credit to get financing on the latest devices..</p>
           </div>
         </div>
       </div>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -30,9 +30,86 @@ const SimpleSlider = () => {
                         </div>
                         <Slider {...settings}>
                             <div>
+                                <h3>SMS Application</h3>
+                                <h4>Software</h4>
+                                <h4>Tools used: Java,
+                                    Spring Framework,
+                                    SpringBoot,
+                                    WebServices,
+                                    SQL and PLSQL,</h4>
+                                <ul>
+                                    <li>
+                                        This platform is a Secure, High Density & Turn‐Key Infrastructure for Service Providers who use its open and scalable architecture to run profitable SMS operations globally.
+                                    </li>
+                                    <li>
+                                        The Platform supports extensive SMPP / HTTP / XML Interoperability, delivers service assurance and enables scalable, reliable and secured connectivity between SMS Wholesale Carriers and Applications.
+                                    </li>
+                                    <li>
+                                        The Platform can be provisioned and managed through web based Interfaces on PCs and Mobiles.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Sella Tracker</h3>
+                                <h4>Web Application</h4>
+                                <h4>
+                                    Tools used: JAVA,SQL,PLSQL,Spring,Hibernate,J2EE and Rest Services
+                                </h4>
+                                <ul>
+                                    <li>
+                                        Responsible for a tracking system application
+                                        which tracks the shipments from source to
+                                        receiving end.Implementing solutions which
+                                        impacts millions of user's data
+                                    </li>
+                                    <li>
+                                        Pulled home data from a PostgreSQL database, and then incorporated
+                                        Google Maps Platform to enhance the users experience of home
+                                        searching.
+                                    </li>
+                                    <li>
+                                        Implemented and Maintained a Digital Signature
+                                        Application for signing pdf.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>5G Home</h3>
+                                <h4>Web and Mobile Application</h4>
+                                <h4>
+                                    Tools used: Java,
+                                    Spring Framework,
+                                    SpringBoot,
+                                    WebServices,
+                                    SQL and PLSQL,
+                                    MicroServices,
+                                    JPA and Hibernate,
+                                    J2EE,
+                                    Design Patterns
+                                </h4>
+                                <ul>
+                                    <li>
+                                        5G Home is end user app which is introduced by
+                                        verizon, where customer can do a self setup of 5G
+                                        device in their location. App guides to users to
+                                        complete the setup with good signal and
+                                        manage the connected devices.
+                                    </li>
+                                    <li>
+                                        Revised, modularized and updated old code
+                                        bases to modern development standards,
+                                        reducing operating costs and improving
+                                        functionality.
+                                    </li>
+                                    <li>
+                                        Client's cache is updated accordingly to improve loading speed.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
                                 <h3>Weather App</h3>
                                 <h4>Web Application</h4>
-                                <h4>Tools used: NodeJS, Express, Handlebars JS, HTML, CSS</h4>
+                                <h4>Tools used: HTML,CSS,ReactJS,JAVA,SpringBoot,SQL,DataStructures</h4>
                                 <ul>
                                     <li>
                                         A web-based application that could reveal the current week weather
@@ -47,81 +124,6 @@ const SimpleSlider = () => {
                                         The web page is fully responsive in desktop, tablet, and mobile
                                         views.
                                     </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h3>Blue Fin</h3>
-                                <h4>Web Application</h4>
-                                <h4>
-                                    Tools used: JavaScript, Python, Django, PostgreSQL, Jinja, HTML,
-                                    Bootstrap CSS
-                                </h4>
-                                <ul>
-                                    <li>
-                                        A Zillow clone web app. Users can create their accounts complete
-                                        with authentication, and search for homes eiter using a search bar
-                                        with filters or an integrated Google Map Platform.
-                                    </li>
-                                    <li>
-                                        Pulled home data from a PostgreSQL database, and then incorporated
-                                        Google Maps Platform to enhance the users experience of home
-                                        searching.
-                                    </li>
-                                    <li>
-                                        Utilized the Django Framework administrative features so both
-                                        client and administrators could login equipped with their own
-                                        privelleges. Client could message realtors, and in doing so would
-                                        also automatically send an e-mail to the realtor.
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h3>Stratus Beats</h3>
-                                <h4>Web Application</h4>
-                                <h4>
-                                    Tools used: ReactJS, Django, GraphQL, Graphene, Apollo Client,
-                                    Insomnia
-                                </h4>
-                                <ul>
-                                    <li>
-                                        A CRUD application that allows users to share their music online
-                                        with other users. The app features social interactions such as
-                                        likes and comments.
-                                    </li>
-                                    <li>
-                                        Hooked up Cloudinary upload API as the method of choice for users
-                                        to Store their music files. Authentication is required to use the
-                                        app and each user has their own profile complete with their list
-                                        of uploaded musics and a list of songs that they liked.
-                                    </li>
-                                    <li>
-                                        Client's cache is updated accordingly to improve loading speed.
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h3>Muscle Pro</h3>
-                                <h4>Web Application (UI Developer)</h4>
-                                <h4>
-                                    Tools used: LESS, HTML, React JS, Node JS
-                                </h4>
-                                <ul>
-                                    <li>
-                                        A fitness tracking-app that helps users manage their workout.
-                                        Users can add workout schemes into the app and keep record of all
-                                        their set, reps, and weights from their training.
-                                    </li>
-                                    <li>
-                                        Users have to authenticate themselves to access their training
-                                        profiles. User information is saved over in the database to keep
-                                        track of all their training and their lifting plans.
-                                    </li>
-                                    <li>
-                                        Utilized LESS preprocessor to style the app. Followed
-                                        preprocessing principles and took advantage of features such as
-                                        mixins, variables, nesting, and imports.
-                                    </li>
-                                   
                                 </ul>
                             </div>
                         </Slider>
